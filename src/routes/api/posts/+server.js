95f343b3af0f13ed9386c5ccfda938a91b64230c
mkg1 <gehring.mg@gmail.com>
2023-12-09 17:32:29 -0500

    Build out get endpoint

--- src/routes/api/posts/+server.js
+++ src/routes/api/posts/+server.js
@@ -1,3 +1,12 @@
-export const GET = () => {
-    return new Response('Howdy, world.')
+import { fetchMarkdownPosts } from '$lib/utils';
+import { json } from '@sveltejs/kit';
+
+// ideally, add try/catch block in case something goes wrong
+export const GET = async () => {
+    const allPosts = await fetchMarkdownPosts();
+    const sortedPosts = allPosts.sort((a, b) => {
+        return new Date(b.meta.date) - new Date(a.meta.date);
+    });
+
+    return json(sortedPosts);
 }
